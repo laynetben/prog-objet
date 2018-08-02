d12b285d1636fb585656d6f387e2c7bb90af27cf
laynetben <42000104+laynetben@users.noreply.github.com>
2018-08-02 13:10:12 +0200

    Add files via upload

---
+++ ArrayList__Program du 31_07_2018.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var listeChaine = new List<string>();
+                      
+            Console.WriteLine("Nom?");
+            listeChaine.Add(Console.ReadLine());
+            Console.Clear();
+
+            Console.WriteLine("Prenom?");
+            listeChaine.Add(Console.ReadLine());
+            Console.Clear();
+
+            Console.WriteLine("Age?");
+            listeChaine.Add(Console.ReadLine());
+            Console.Clear();
+
+            Console.WriteLine("Bonjour " +listeChaine[1]+ " " +listeChaine[0]+",\nTu as " +listeChaine[2]+ " "+ "ans.");
+                       
+        
+            Console.ReadKey();
+        }
+    }
+}
